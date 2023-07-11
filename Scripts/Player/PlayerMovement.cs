774a537b58058bdb6a00a5d10b6670a39330260f
Gabril <gabrielgcrepaldi@gmail.com>
2023-07-11 11:16:14 -0300

    Started implementing bunny hop

--- Scripts/Player/PlayerMovement.cs
+++ Scripts/Player/PlayerMovement.cs
@@ -11,7 +11,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpSpeed;
     [SerializeField] Transform orientation;
-    [SerializeField] float maxSpeed;
     //[SerializeField] float crouchSpeed;
     //[SerializeField] float jumpCooldown;
 
@@ -22,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     Vector3 direction;
 
     private float crouchYScale, startYScale;
+
     //ground check parameters
     [SerializeField] float groundDrag;
     [SerializeField] LayerMask groundLayer;
@@ -31,9 +31,16 @@ public class PlayerMovement : MonoBehaviour
     private float maxSlopeAngle = 50;
     private RaycastHit slopeHit;
 
+    //Bunny Hop
+    private float timeTouchedGround;
+    private float originalMoveSpeed;
+    private float bunnyHopBuffer = 0.5f;
+
     //booleans
     private bool isGrounded;
     private bool isJumpReady;
+    private bool isCrounching;
+    private bool justTouchedGround = false;
 
     void Start()
     {
@@ -41,37 +48,57 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
         playerHeight = rb.transform.localScale.y*2;
         startYScale = transform.localScale.y;
+        originalMoveSpeed = moveSpeed;
     }
 
     void Update()
     {
+        Debug.Log(rb.velocity.y);
+        //Debug.Log(Time.time < timeTouchedGround + bunnyHopBuffer);
         groundCheck();
         move();
+        
     }
 
     private void inputAssignor() { //make inputs checks and assign them to control variables
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
+        
         if (Input.GetKey(KeyCode.Space) && isGrounded) {  //&& isJumpReady) {
-            //isJumpReady = false;
+            isJumpReady = false;
+            if (Time.time < timeTouchedGround + bunnyHopBuffer)
+            {
+               if(moveSpeed < originalMoveSpeed * 2.5f) {
+                   moveSpeed += 2;
+                }
+                else
+                {
+                    moveSpeed = originalMoveSpeed;
+                }
+
+            }          
             jump();
             //Invoke(() => { isJumpReady = true; }, jumpCooldown);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftControl)) {
             crouch();
+            moveSpeed = moveSpeed * 1.5f;
+            isCrounching = true;
         }
 
         if (Input.GetKeyUp(KeyCode.LeftControl))
         {
+            moveSpeed = moveSpeed / 1.5f;
             transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+            isCrounching = false;
         }
     }
 
     private void move() {
         inputAssignor();
-        speedLimiter();
+        
 
         direction = orientation.forward * verticalInput + orientation.right * horizontalInput; //gets the direction by combining both inputs with the rotation
 
@@ -89,6 +116,7 @@ public class PlayerMovement : MonoBehaviour
         }
         
         rb.useGravity = !onSlope();
+        speedLimiter();
     }
 
     private void groundCheck() {
@@ -96,40 +124,48 @@ public class PlayerMovement : MonoBehaviour
 
         if (isGrounded)
         {
+            if (!justTouchedGround){
+                timeTouchedGround = Time.time;
+                justTouchedGround = true;
+            }
             rb.drag = groundDrag;
+            
         }
         else {
             rb.drag = 0;
         }
     }
 
-    private void speedLimiter() {
-        if (onSlope())
-        {
-            if (rb.velocity.magnitude > moveSpeed)
-            {
+    private void speedLimiter(){
+        Vector3 flatSpeed = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if (onSlope()){
+            if (rb.velocity.magnitude > moveSpeed){
                 rb.velocity = rb.velocity.normalized * moveSpeed;
             }
         }
-        else {
-            if (rb.velocity.magnitude > maxSpeed)
+        else
+        {
+            if (flatSpeed.magnitude > moveSpeed)
             {
-                rb.velocity = rb.velocity.normalized * maxSpeed;
+                
+                Vector3 limitedSpeed = flatSpeed.normalized * moveSpeed;
+                rb.velocity = new Vector3(limitedSpeed.x, rb.velocity.y, limitedSpeed.z);
             }
         }
-        
     }
     private void jump() {
 
             rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //resets the y velocity to prevent force conflicts
 
             rb.AddForce(transform.up * jumpSpeed, ForceMode.Impulse);
+            justTouchedGround = false;
         
     }
 
     private void crouch() {
         transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
         rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        rb.AddForce(direction * 1000, ForceMode.Force);
     }
 
     private bool onSlope() {
